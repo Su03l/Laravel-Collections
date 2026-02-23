38c1a288bf7452a4cdeed72d68930d13db604ee2
Suliman <salmanyousef13@gmail.com>
2026-02-23 05:29:29 +0300

    some update

--- hospital-app/app/Http/Middleware/IsAdmin.php
+++ hospital-app/app/Http/Middleware/IsAdmin.php
@@ -8,11 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+
+    // the middleware handler for the route
     public function handle(Request $request, Closure $next): Response
     {
         if (auth()->check() && auth()->user()->role === 'admin') {
