dc975d6ed025670cbaa8f8b31dbc512b15b4ef10
Suliman <salmanyousef13@gmail.com>
2026-02-24 19:07:25 +0300

    someupdate

--- hospital-app/database/migrations/0001_01_01_000002_create_jobs_table.php
+++ hospital-app/database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -43,9 +43,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('jobs');
