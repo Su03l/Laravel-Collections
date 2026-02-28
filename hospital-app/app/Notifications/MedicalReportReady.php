cbef381c9566520865283dbdedeb930508212dea
Suliman <salmanyousef13@gmail.com>
2026-02-28 09:13:19 +0300

    some update

--- hospital-app/app/Notifications/MedicalReportReady.php
+++ hospital-app/app/Notifications/MedicalReportReady.php
@@ -14,6 +14,7 @@ class MedicalReportReady extends Notification implements ShouldQueue
 
     protected $record;
 
+    // constructor
     public function __construct(MedicalRecord $record)
     {
         $this->record = $record;
