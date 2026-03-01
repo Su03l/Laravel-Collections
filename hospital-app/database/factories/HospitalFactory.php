adfd203289d0a062e65e47ffe436aee7a20698b9
Suliman <salmanyousef13@gmail.com>
2026-03-01 08:21:09 +0300

    some update

--- hospital-app/database/factories/HospitalFactory.php
+++ hospital-app/database/factories/HospitalFactory.php
@@ -19,7 +19,7 @@ class HospitalFactory extends Factory
         return [
             'name' => $this->faker->company() . ' Hospital', // Random hospital name
             'address' => $this->faker->address(), // Random address
-            'city' => $this->faker->randomElement(['الرياض', 'جدة', 'الدمام', 'مكة']),
+            'city' => $this->faker->randomElement(['الرياض', 'جدة', 'الدمام', 'مكة']), // Random city
             'image' => 'https://via.placeholder.com/400x300.png?text=Hospital',
         ];
     }
