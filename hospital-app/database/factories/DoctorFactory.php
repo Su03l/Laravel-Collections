d8b6f4ae524d62e5d1412f8556ff2bc9e3c67b21
Suliman <salmanyousef13@gmail.com>
2026-03-01 08:20:18 +0300

    some update

--- hospital-app/database/factories/DoctorFactory.php
+++ hospital-app/database/factories/DoctorFactory.php
@@ -31,7 +31,7 @@ class DoctorFactory extends Factory
             'clinic_id' => Clinic::factory(), // clinic factory
             'name' => $user->name, // استخدام نفس الاسم
             'specialization' => $this->faker->randomElement(['جراحة قلب', 'طب أطفال', 'أسنان', 'جلدية']), // Random specialization
-            'bio' => $this->faker->paragraph(),
+            'bio' => $this->faker->paragraph(), // Random bio
             'experience_years' => $this->faker->numberBetween(2, 25),
             'image' => 'https://via.placeholder.com/200x200.png?text=Doctor',
             'is_active' => true,
