6769db8ea17548648707a3affe4c089d7754b62b
Suliman <salmanyousef13@gmail.com>
2026-03-01 08:23:37 +0300

    some update

--- hospital-app/database/seeders/AdminSeeder.php
+++ hospital-app/database/seeders/AdminSeeder.php
@@ -14,6 +14,7 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
+        // create admin account
         User::create([
             'name' => 'Admin Manager',
             'email' => 'admin@hospitalsync.com',
