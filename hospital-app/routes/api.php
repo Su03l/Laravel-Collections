cf99e6ae4e209feb63e1675215472f18b44d6945
Suliman <salmanyousef13@gmail.com>
2026-03-04 09:09:43 +0300

    all done

--- hospital-app/routes/api.php
+++ hospital-app/routes/api.php
@@ -28,15 +28,10 @@ use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| 1. المسارات العامة (Public Routes)
-|--------------------------------------------------------------------------
-*/
 
 // Auth
 Route::middleware('throttle:10,1')->group(function () {
-    Route::post('/register', RegisterController::class);
+    Route::post('/register', RegisterController::class); // Register
     Route::post('/login', LoginController::class);
     Route::post('/forgot-password', ForgotPasswordController::class);
     Route::post('/reset-password', ResetPasswordController::class);
