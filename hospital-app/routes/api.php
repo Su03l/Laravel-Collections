c071cd9ebf6180e0abf889e56ec0ab46ffdb123c
Suliman <salmanyousef13@gmail.com>
2026-03-04 09:09:57 +0300

    all done

--- hospital-app/routes/api.php
+++ hospital-app/routes/api.php
@@ -33,7 +33,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('throttle:10,1')->group(function () {
     Route::post('/register', RegisterController::class); // Register
     Route::post('/login', LoginController::class); // Login
-    Route::post('/forgot-password', ForgotPasswordController::class);
+    Route::post('/forgot-password', ForgotPasswordController::class); // Forgot Password
     Route::post('/reset-password', ResetPasswordController::class);
 });
 
