ee63f922dd99a125459cb70c7b368608bc8f9294
Suliman <salmanyousef13@gmail.com>
2026-03-04 09:02:13 +0300

    all done

---
+++ hospital-app/app/Traits/ApiResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse {
+
+    public function success($data = null, $message = 'Operation successful', $code = 200) {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    public function error($data = null, $message = 'An error occurred', $code = 400) {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'errors' => $data
+        ], $code);
+    }
+}
