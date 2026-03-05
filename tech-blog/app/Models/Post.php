a9bfd8146738a7d1172045e6ecf990a8cd752a6f
Suliman <salmanyousef13@gmail.com>
2026-03-05 09:41:54 +0300

    some update

--- tech-blog/app/Models/Post.php
+++ tech-blog/app/Models/Post.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    protected $fillable = ['user_id', 'title', 'slug', 'content'];
+
+    // المقال ينتمي لكاتب واحد
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // المقال يحتوي على عدة تعليقات
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    // المقال يحتوي على عدة مرفقات (صور/ملفات)
+    public function attachments()
+    {
+        return $this->hasMany(Attachment::class);
+    }
+
+    // المقال يرتبط بعدة هاشتاقات (علاقة Many-to-Many)
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
