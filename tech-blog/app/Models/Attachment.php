aa6f3c979e9d4dd7a9a16e2771dc2cebf0df21e6
Suliman <salmanyousef13@gmail.com>
2026-03-05 09:42:09 +0300

    some update

--- tech-blog/app/Models/Attachment.php
+++ tech-blog/app/Models/Attachment.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    use HasFactory;
+    protected $fillable = ['post_id', 'file_path', 'file_name', 'file_type'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
