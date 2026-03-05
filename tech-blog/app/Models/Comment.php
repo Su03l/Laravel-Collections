dd7f53b479863ecf1e5437187de7a6dda48cc2db
Suliman <salmanyousef13@gmail.com>
2026-03-05 10:04:48 +0300

    some update

--- tech-blog/app/Models/Comment.php
+++ tech-blog/app/Models/Comment.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['user_id', 'post_id', 'content'];
+    // أضفنا الـ parent_id هنا
+    protected $fillable = ['user_id', 'post_id', 'parent_id', 'content'];
 
     public function user()
     {
@@ -16,5 +17,23 @@ class Comment extends Model
     public function post()
     {
         return $this->belongsTo(Post::class);
+    }
+
+    // علاقة اللايكات للتعليق (Polymorphic)
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    // الردود التابعة لهذا التعليق (الأبناء)
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    // التعليق الأساسي (الأب)
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
     }
 }
