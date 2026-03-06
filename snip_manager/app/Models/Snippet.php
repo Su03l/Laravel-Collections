9bfe1f60c98130c34d2f7c7a08af9736f3a0add1
Suliman <salmanyousef13@gmail.com>
2026-03-06 09:19:26 +0300

    some update

--- snip_manager/app/Models/Snippet.php
+++ snip_manager/app/Models/Snippet.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Snippet extends Model
 {
-    //
+    protected $fillable = ['category_id', 'title', 'code', 'description', 'language'];
+
+    // الكود ينتمي لقسم واحد
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
