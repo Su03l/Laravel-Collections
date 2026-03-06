f4017b3289f881c9e7833245ae97a331def99156
Suliman <salmanyousef13@gmail.com>
2026-03-06 09:19:19 +0300

    some update

--- snip_manager/app/Models/Category.php
+++ snip_manager/app/Models/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name', 'slug', 'color'];
+
+    // القسم يملك عدة أكواد
+    public function snippets()
+    {
+        return $this->hasMany(Snippet::class);
+    }
 }
