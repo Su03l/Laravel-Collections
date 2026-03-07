7c7ca9c3cd3074d33175a28f33ad182a51f753ac
Suliman <salmanyousef13@gmail.com>
2026-03-07 09:42:04 +0300

    some update

--- mini-erp/routes/web.php
+++ mini-erp/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// مسارات لوحة التحكم (محمية بتسجيل الدخول وبصلاحية المدير)
+Route::middleware(['auth', 'admin'])->group(function () {
+
+    // سطر واحد فقط يبني لك كل الروابط (index, create, store, edit, update, destroy)
+    Route::resource('employees', EmployeeController::class);
+
 });
