f7f686df28dc2ff8afbe937a8f1a7c55fa4ea6c7
Suliman <salmanyousef13@gmail.com>
2026-03-08 10:33:47 +0300

    some update

--- mini-erp/app/Http/Controllers/AuthController.php
+++ mini-erp/app/Http/Controllers/AuthController.php
@@ -31,7 +31,6 @@ class AuthController extends Controller
                 return redirect()->intended(route('employees.index'));
             }
 
-            // إذا كان موظف عادي، نوجهه للوحة الخاصة فيه
             return redirect()->intended(route('employee.dashboard'));
         }
 
