3ad989b3dde3f502dba70367beb374900adc23dd
Suliman <salmanyousef13@gmail.com>
2026-03-08 10:33:41 +0300

    some update

--- mini-erp/app/Http/Controllers/AuthController.php
+++ mini-erp/app/Http/Controllers/AuthController.php
@@ -26,6 +26,7 @@ class AuthController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
+            // التوجيه الذكي بناءً على الصلاحية (Role-based Redirect)
             if (auth()->user()->role === 'admin') {
                 return redirect()->intended(route('employees.index'));
             }
