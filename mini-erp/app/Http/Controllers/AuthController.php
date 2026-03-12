fba1043a530f442a49ceb4f91e3b2271aab88ef7
Suliman <salmanyousef13@gmail.com>
2026-03-12 04:59:07 +0300

    some update

--- mini-erp/app/Http/Controllers/AuthController.php
+++ mini-erp/app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    // 1. عرض صفحة تسجيل الدخول
     public function showLoginForm()
     {
         return view('auth.login');
